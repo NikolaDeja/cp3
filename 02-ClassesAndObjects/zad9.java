53213511e57444ecdaf360d033ce3d7169a5afeb
Nikola Deja <nikolaa.deja@gmail.com>
2024-10-10 19:16:41 +0200

    before class

---
+++ 02-ClassesAndObjects/zad9.java
@@ -0,0 +1,19 @@
+public class zad9
+{
+    public static void main(String[] args)
+    {
+        //name, surname, age, student id, whether you are a first-year student or not,
+        // field of study and the name of the university where you study
+        String name="Nikola", field="Applied Informatics", university="UEK";
+        int age=19, student_id=231696;
+        boolean first_year= false;
+
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Student ID: " + student_id);
+        System.out.println("First year student: " + first_year);
+        System.out.println("Field of study: " + field);
+        System.out.println("University: " + university);
+    }
+}
